93eb54bc826425d51e660c3e3f535a52d8f9150f
DBsMOJO <d.binder1@students.htl-leonding.ac.at>
2023-12-12 08:04:55 +0100

    add code

---
+++ HelloWorld.c
@@ -0,0 +1,17 @@
+/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: <CLASSNAME>
+ *--------------------------------------------------------------
+ *              Daniel Binder, 12.12.2023
+ *--------------------------------------------------------------
+ * Description:
+ * make a Hallo-Wolrd file
+ *--------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main() {
+  // printf() displays the string inside quotation
+  printf("Hello, World!\n");
+  return 0;
+}
